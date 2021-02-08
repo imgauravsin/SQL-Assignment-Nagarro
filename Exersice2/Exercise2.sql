66dc8138b9b6a55bd4538a45e785c1a5046b37e3
GAURAV SINGH <42248264+imgauravsin@users.noreply.github.com>
2021-02-08 17:24:01 +0530

    Create Exercise2.sql

---
+++ Exersice2/Exercise2.sql
@@ -0,0 +1,26 @@
+use AdventureWorks2008R2;
+-- Using Join
+Select C.CustomerID from Sales.SalesOrderHeader SOH
+Right Join Sales.Customer C
+on C.CustomerID=SOH.CustomerID
+Where SalesOrderID is NULL;
+
+-- Using Subquery
+Select CustomerID from Sales.Customer C  
+Where C.CustomerID  
+Not in (Select CustomerID from Sales.SalesOrderHeader);
+
+-- EXIST
+Select C.CustomerID From Sales.Customer C
+Where NOT EXISTS (Select s.CustomerID from Sales.SalesOrderHeader as s where s.CustomerID=C.CustomerID); 
+
+-- CTE(Common Table Expression)
+With CustomerWithoutOrder(CustomerID)
+As
+
+(Select C.CustomerID from Sales.SalesOrderHeader SOH
+Right Join Sales.Customer C
+on C.CustomerID=SOH.CustomerID
+Where SalesOrderID is NULL)
+
+Select * from CustomerWithoutOrder;
